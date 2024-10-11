0c37ec9108ce5d13519382f9cdcae1f8cd3d1510
ArcoKim <arcomade12@gmail.com>
2024-10-11 14:59:44 +0900

    app, data update

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+kakao_api_key = os.environ.get('KAKAO_API_KEY')
+
 @app.route('/')
 def index():
-    api_key = os.environ.get('KAKAO_API_KEY')
-    return render_template('index.html', api_key=api_key)
+    return render_template('index.html', api_key=kakao_api_key)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
