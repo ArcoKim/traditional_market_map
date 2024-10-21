e7ba64805d16ba5f374a82f14e1e9acdbc0b0cbe
ArcoKim <arcomade12@gmail.com>
2024-10-21 14:49:45 +0900

    Make Market Sidebar

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
 import os
-import pymysql
-import pymysql.cursors
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 kakao_api_key = os.environ.get('KAKAO_API_KEY')
 
 def get_connection():
-    return pymysql.connect(
-        host=os.environ.get('DB_HOST'),
-        user=os.environ.get('DB_USER'),
-        password=os.environ.get('DB_PASSWORD'),
-        database=os.environ.get('DB_DATABASE'),
-        cursorclass=pymysql.cursors.DictCursor
-    )
-
+    conn = sqlite3.connect('data/tmmap.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+    
 @app.route('/')
 def index():
     return render_template('index.html', api_key=kakao_api_key)
@@ -26,17 +21,40 @@ def position():
     ret = []
 
     try:
-        with conn.cursor() as cursor:
-            sql = "SELECT name, latitude, longitude FROM market"
-            cursor.execute(sql)
+        cur = conn.cursor()
+        cur.execute("SELECT name, latitude, longitude FROM market")
 
-            results = cursor.fetchall()
-            for row in results:
-                ret.append(row)
+        results = cur.fetchall()
+        for row in results:
+            ret.append(dict(row))
     finally:
         conn.close()
 
     return ret
+
+@app.route('/market')
+def market():
+    latitude = request.args.get('latitude')
+    longitude = request.args.get('longitude')
+
+    conn = get_connection()
+
+    try:
+        cur = conn.cursor()
+        cur.execute("""
+        SELECT category, address, open_cycle, number_of_stores, items_type, homepage, open_year, phone_number, restroom, arcade, elevator, support_center, 
+        sprinkler, fire_detector, play_room, call_center, foyer, lactation_center, stockroom, bicycle_rack, gym, library, shopping_cart, visitor_center, 
+        movement_path, broadcasting_center, cultural_center, cooperative_warehouse, parking, education_center, meeting_room, AED
+        FROM market, facility
+        WHERE market.facility_id = facility.id
+        AND market.latitude = ? AND market.longitude = ?
+        """, (latitude, longitude))
+
+        ret = cur.fetchone()
+    finally:
+        conn.close()
+
+    return dict(ret)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
--- static/javascript/script.js
+++ static/javascript/script.js
@@ -6,13 +6,12 @@ const mapOption = {
 
 const map = new kakao.maps.Map(mapContainer, mapOption);
 
-async function getPosition() {
+async function fetching(path) {
     try {
-        const response = await fetch('/position');
+        const response = await fetch(path);
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
-
         const ret = await response.json();
         return ret;
     } catch (error) {
@@ -20,18 +19,37 @@ async function getPosition() {
     }
 }
 
-getPosition().then(positions => {
-    positions.map(position => {
-        return new Promise(resolve => {
-            new kakao.maps.Marker({
+async function initializeMarkers() {
+    const positions = await fetching('/position');
+
+    if (positions) {
+        positions.forEach(position => {
+            const marker = new kakao.maps.Marker({
                 map: map,
                 position: new kakao.maps.LatLng(position['latitude'], position['longitude']),
                 title: position['name']
             });
-            resolve();
+
+            kakao.maps.event.addListener(marker, 'click', async () => { 
+                const market = await fetching(`/market?latitude=${position['latitude']}&longitude=${position['longitude']}`)
+                document.getElementById('market_name').textContent = position['name'];
+                for(let [key, value] of Object.entries(market)) {
+                    if(value === 1) {
+                        value = '보유함'
+                    } else if(value === 0) {
+                        value = '보유하지 않음'
+                    } else if(!value) {
+                        value = '정보 없음'
+                    }
+                    document.getElementById(key).textContent = value;
+                }
+                document.getElementById('info').style.display = 'block';
+            });
         });
-    });
-});
+    }
+}
+
+initializeMarkers();
 
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {        
@@ -54,3 +72,7 @@ if (navigator.geolocation) {
         infowindow.open(map, marker);
     });
 }
+
+document.getElementById('close').addEventListener('click', () => {
+    document.getElementById('info').style.display = 'none';
+});
